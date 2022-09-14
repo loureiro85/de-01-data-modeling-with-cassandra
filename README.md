0bc42d8f83d8fabbc368054cd68dfc7d7e1a3dc6
Gui-laptop <guilhermel@kaspien.com>
2022-09-14 16:45:09 -0300

    fix: Fix code based on project rubric.

--- README.md
+++ README.md
@@ -4,3 +4,10 @@ Data Modeling with Apache Cassandra - Udacity's Nanodegree Project
 ## Setup - Cassandra installation
 
 https://cassandra.apache.org/doc/latest/cassandra/getting_started/installing.html
+
+# Project description
+
+- Implement modular and clean ETL pipeline in Python (`project_1B_data_modeling_with_cassandra.ipynb`)
+- Model Apache Cassandra database based on desired queries
+- Read data from CSV directory and insert into tables
+- Query data
--- queries.py
+++ queries.py
@@ -4,23 +4,23 @@ that was heard during sessionId = 338, and itemInSession = 4"""
 
 QUERY_1 = """
     SELECT artist, song_title, song_length
-    FROM event_1
+    FROM song_by_item
     WHERE session_id=338 AND item_in_session=4;
     """
 
 CREATE_TABLE_1 = """
-    CREATE TABLE IF NOT EXISTS event_1 (
+    CREATE TABLE IF NOT EXISTS song_by_item (
+        session_id int,
+        item_in_session int,
         artist text,
         song_title text,
         song_length float,
-        session_id int,
-        item_in_session int,
         PRIMARY KEY (session_id, item_in_session)
     )
 """
 
 INSERT_1 = """
-    INSERT INTO event_1 (
+    INSERT INTO song_by_item (
         artist,
         song_title,
         song_length,
@@ -42,26 +42,26 @@ for userid = 10, sessionid = 182
 
 QUERY_2 = """
     SELECT artist, song_title, first_name, user_last_name
-    FROM event_2
+    FROM song_by_user_and_session
     WHERE user_id=10 AND session_id=182
     ORDER BY item_in_session;
     """
 
 CREATE_TABLE_2 = """
-    CREATE TABLE IF NOT EXISTS event_2 (
+    CREATE TABLE IF NOT EXISTS song_by_user_and_session (
+        user_id int,
+        session_id int,
+        item_in_session int,
         artist text,
         song_title text,
         first_name text,
         user_last_name text,
-        user_id int,
-        session_id int,
-        item_in_session int,
         PRIMARY KEY (user_id, session_id, item_in_session)
     )
 """
 
 INSERT_2 = """
-    INSERT INTO event_2 (
+    INSERT INTO song_by_user_and_session (
         artist,
         song_title,
         first_name,
@@ -80,21 +80,21 @@ to the song 'All Hands Against His Own'"""
 
 QUERY_3 = """
     SELECT first_name, user_last_name
-    FROM event_3
+    FROM user_by_song
     WHERE song_title='All Hands Against His Own';
     """
 
 CREATE_TABLE_3 = """
-    CREATE TABLE IF NOT EXISTS event_3 (
+    CREATE TABLE IF NOT EXISTS user_by_song (
+        song_title text,
         first_name text,
         user_last_name text,
-        song_title text,
         PRIMARY KEY (song_title, first_name, user_last_name)
         )
 """
 
 INSERT_3 = """
-    INSERT INTO event_3 (
+    INSERT INTO user_by_song (
         first_name,
         user_last_name,
         song_title
